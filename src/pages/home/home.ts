90c666ec0cd97c94654a32f5927e20e497e8d50e
Craig Turner <cragster33@hotmail.com>
2016-12-21 23:32:14 +0000

    Added service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController} from 'ionic-angular';
+import { NestService } from '../services/NestService';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [NestService]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nests: [{}];
+
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public alertCtrl: AlertController, private nestService: NestService) {
 
   }
 
@@ -21,5 +24,39 @@ export class HomePage {
     "image":"025 - BnOQ18z.png",
     "location":"Woodthorpe Park"
   }];
+
+  getNests() {
+    this.nestService.listNests().subscribe(
+        data => {
+            this.nests = data.results;
+            console.log(data.results);
+        },
+        err => {
+            console.log(err);
+        },
+        () => console.log('Games Search Complete')
+    );
+  }
+
+
+  doRefresh(refresher) {
+     //console.log('Begin async operation', refresher);
+     setTimeout(() => {
+       refresher.complete();
+     }, 2000);
+   }
+
+   showAlert(item) {
+     let alert = this.alertCtrl.create({
+       title: 'Woohoo!',
+       subTitle: 'You selected the title: '+item.title,
+       buttons: ['OK']
+     });
+     alert.present();
+   }
+
+   itemSelected(item) {
+     this.showAlert(item);
+   }
 
 }
---
+++ src/services/NestService.ts
@@ -0,0 +1,58 @@
+import {Http, Headers} from '@angular/http';
+import 'rxjs/Rx';
+
+declare let Parse: any;
+
+export class NestService {
+    static get parameters() {
+        return [[Http]];
+    }
+
+    constructor(private http:Http) {
+
+    }
+
+
+    createAuthorizationHeader(headers: Headers) {
+      headers.append('X-Parse-Application-Id','7iqBN0vAzR7dVUr4SFY4O6kKasPSVmiCNDJVPSSm');
+      headers.append('X-Parse-REST-API-Key', 'kRpwLQ1BAd85ow635D7bDQOhSADsViCSmPi5SVzP');
+
+    }
+
+
+    listNests() {
+
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+
+        var query = 'include=pokemon';
+        var url = 'https://pg-app-237jd14w1ijbdxxfdfdhyiea0fy3bh.scalabl.cloud/1/classes/Nests?'+query;
+        var response = this.http.get(url, {
+          headers: headers
+        }).map(res => res.json());
+		    return response;
+    }
+
+    registerUser(email: string, password: string, birthday?: any) {
+      console.log('registering ' + email);
+      var user = new Parse.User();
+      user.set("username", email);
+      user.set("email", email);
+      user.set("password", password);
+
+      // other fields can be set just like with Parse.Object
+      user.set("birthday", birthday);
+
+      user.signUp(null, {
+        success: function (user) {
+          console.log('success');
+        },
+        error: function (user, error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
+
+
+}
