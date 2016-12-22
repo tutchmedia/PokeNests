a6508f6c10ec5c4a8605319d07895626d5ff3f73
Craig Turner <cragster33@hotmail.com>
2016-12-22 23:41:16 +0000

    Status bar -> Light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.styleLightContent();
       Splashscreen.hide();
 
       // Get to show for IOS only
