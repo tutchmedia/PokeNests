2e2dcc2a182ee0d0cf171489b59401df18a355a5
Craig Turner <cragster33@hotmail.com>
2016-12-22 22:55:30 +0000

    Added map to detail page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import {
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapsLatLng,
+ CameraPosition,
+ GoogleMapsMarkerOptions,
+ GoogleMapsMarker
+} from 'ionic-native';
 // Services
 import { NestService } from '../../services/NestService';
 
@@ -33,6 +41,51 @@ export class ContactPage {
         },
         () => console.log('Games Search Complete')
     );
+  }
+
+
+  // Load and init the map
+  ngAfterViewInit() {
+   this.loadMap();
+  }
+
+  loadMap() {
+     // make sure to create following structure in your view.html file
+     // <ion-content>
+     //  <div #map id="map"></div>
+     // </ion-content>
+
+     // create a new map by passing HTMLElement
+     let element: HTMLElement = document.getElementById('map');
+
+     let map = new GoogleMap(element);
+
+     // listen to MAP_READY event
+     map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
+
+     // create LatLng object
+     let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+
+     // create CameraPosition
+     let position: CameraPosition = {
+       target: ionic,
+       zoom: 18,
+       tilt: 30
+     };
+
+     // move the map's camera to position
+     map.moveCamera(position);
+
+     // create new marker
+     let markerOptions: GoogleMapsMarkerOptions = {
+       position: ionic,
+       title: 'Ionic'
+     };
+
+     map.addMarker(markerOptions)
+       .then((marker: GoogleMapsMarker) => {
+          marker.showInfoWindow();
+        });
   }
 
 }
